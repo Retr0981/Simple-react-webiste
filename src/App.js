2649f11013941355b3a94d1defc5de10b1f09aa5
David Nii Armah <davidarmah991@gmail.com>
2022-05-20 03:52:08 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+
+function App() {
+  return (
+   <div className="App">
+   <h2>Hi I am David Nii Armah 🚀</h2>
+
+   <h2>Hi I am DNA </h2>
+   <h2> I am a King </h2>
+   <h1 
+   style={{
+    backgroundColor: "yellow",
+    height: "200px",
+    width: "200px",
+    border: "15px dotted green",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "blue",
+   }}>
+   Let's increase the coding Dopamine
+   </h1>
+   </div>
+);
+}
+
+export default App;
